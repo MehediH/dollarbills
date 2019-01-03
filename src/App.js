ce0ccebb059f6818bf47322b1bf7304f4894f65d
Mehedi Hassan <mehedi.dip@outlook.com>
2019-01-03 21:55:44 +0000

    she complain she can't see me often

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
-import { resolve } from 'path';
+import loader from "./loader.svg";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      author: "948",
       posts: [],
       postCount: 0,
       shiftcount: 0,
+      display: false,
       previousMonth: {
         posts: [],
         postCount: 0,
-        shiftcount: 0
+        shiftcount: 0,
+        display: false
       },
       searchMonth: {
         posts: [],
         postCount: 0,
         shiftcount: 0,
-        date: ""
+        date: "",
+        display: true
       },
       searchValues: {
-        month: "",
+        month: "01",
         year: ""
       }
     }
 
+    this.getCurrentMonth = this.getCurrentMonth.bind(this);
+    this.getPreviousMonth = this.getPreviousMonth.bind(this);
     this.search = this.search.bind(this);
+    this.updateAuthor = this.updateAuthor.bind(this);
   }
-  
 
-  componentWillMount() {
-    // current month
+  componentDidMount() { 
+    this.getCurrentMonth();
+    this.getPreviousMonth();    
+  }
+
+  getCurrentMonth(){
     var date = new Date();
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
     var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
@@ -42,12 +52,14 @@ class App extends Component {
 
     this.getDataMonth(firstDay, lastDay).then((data) => {
       this.setState({
-        ...data
+        ...data,
+        display: true
       })
     });
+  }
 
-
-    // previous month
+  getPreviousMonth(){
+    var date = new Date();
     var prevFirstDay = new Date(date.getFullYear(), date.getMonth() - 1, 1);
     var prevLastDay = new Date(date.getFullYear(), date.getMonth(), 1);
 
@@ -58,15 +70,17 @@ class App extends Component {
     this.getDataMonth(prevFirstDay, prevLastDay).then((data) => {
       this.setState({
         previousMonth: {
-          ...data
+          ...data,
+          display: true
         }
       })
     });
   }
 
+
   getDataMonth(start, end){
 
-    return fetch('https://www.thurrott.com/wp-json/wp/v2/posts?author=948&per_page=100&after=' + start + '&before=' + end).then((response) => response.json())
+    return fetch('https://www.thurrott.com/wp-json/wp/v2/posts?author=' + this.state.author + '&per_page=100&after=' + start + '&before=' + end).then((response) => response.json())
       .then((responseJson) => {
                 
         var tempShiftCount = 0;
@@ -106,6 +120,8 @@ class App extends Component {
   }
 
   search(){
+    this.setState({searchMonth: {display: false}});
+    
     var date = this.state.searchValues.year + "-" + this.state.searchValues.month + "-01T00:00:00";
 
     date = new Date(date);
@@ -113,6 +129,7 @@ class App extends Component {
     var searchFirstDay = new Date(date.getFullYear(), date.getMonth(), 1);
     var searchLastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
 
+    
     searchFirstDay = searchFirstDay.toISOString();
     searchLastDay = searchLastDay.toISOString();
 
@@ -120,36 +137,95 @@ class App extends Component {
       this.setState({
         searchMonth: {
           ...data,
-          date: date.toLocaleString('en-us', { month: 'long' }) + ", " + date.getFullYear()
+          date: date.toLocaleString('en-us', { month: 'long' }) + " " + date.getFullYear(),
+          display: true
         }
       })
     });
   }
 
+  updateAuthor(){
+    this.setState({display: false});
+    this.setState({previousMonth: {display: false}});
+
+    if(this.state.searchMonth.postCount !== 0){
+      this.setState({searchMonth: {display: true}});
+    }
+
+    this.getCurrentMonth();
+    this.getPreviousMonth();  
+    
+    if(this.state.searchValues.month !== "" && this.state.searchValues.year !== ""){
+      this.search();
+    }
+  }
+
   render() {
     return (
       <div className="app">
-          <div className="main">
-            <h1>
-              You posted {this.state.postCount} articles this month.
-            </h1>
+          <header>
+            <h1>THURROTT 💸💰</h1>
 
-            <h2>{this.state.shiftcount} of which were between 2am and 7am.</h2>
+          </header>
+          <label>Author ID</label>
+         
+
+          <input type="text" value={this.state.author} onChange={(event) => {this.setState({author: event.target.value})}} />
+          <input type="button" value="submit" onClick={this.updateAuthor}/>
+
+          <div className="main">
+            {
+              this.state.display === false ? <img src={loader} className="App-logo" alt="logo" /> :
+
+              <React.Fragment>
+                <h1>
+                  You posted {this.state.postCount} articles this month.
+                </h1>
+    
+                <h2>{this.state.shiftcount} of which were between 2am and 7am.</h2>
+
+              </React.Fragment>
+
+            }
           </div>
 
           <div className="main">
-            <h1>
-              You posted {this.state.previousMonth.postCount} articles last month.
-            </h1>
 
-            <h2>{this.state.previousMonth.shiftcount} of which were between 2am and 7am.</h2>
+            {
+              this.state.previousMonth.display === false ? <img src={loader} className="App-logo" alt="logo" /> :
+
+              <React.Fragment>
+                <h1>
+                  You posted {this.state.previousMonth.postCount} articles last month.
+                </h1>
+
+                <h2>{this.state.previousMonth.shiftcount} of which were between 2am and 7am.</h2>
+
+              </React.Fragment>
+
+            }
+
           </div>
           
           <div className="main">
             <h1>Search</h1>
 
             <label>month number</label>
-            <input type="text" value={this.state.searchValues.month} onChange={(event) => {this.setState({searchValues: {month: event.target.value, year: this.state.searchValues.year}})}} />
+            <select value={this.state.searchValues.month} onChange={(event) => {this.setState({searchValues: {month: event.target.value, year: this.state.searchValues.year}})}}>
+              <option value="01">January</option>
+              <option value="02">February</option>
+              <option value="03">March</option>
+              <option value="04">April</option>
+              <option value="05">May</option>
+              <option value="06">June</option>
+              <option value="07">July</option>
+              <option value="08">August</option>
+              <option value="09">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+
             
             <label>year</label>
             <input type="text" value={this.state.searchValues.year} onChange={(event) => {this.setState({searchValues: {month: this.state.searchValues.month, year: event.target.value}})}} />
@@ -157,6 +233,8 @@ class App extends Component {
             <input type="button" value="submit" onClick={this.search}/>
 
             {
+              this.state.searchMonth.display === false ? <img src={loader} className="App-logo" alt="logo" /> :
+
               this.state.searchMonth.postCount != 0 &&
               
               <React.Fragment>
@@ -166,7 +244,10 @@ class App extends Component {
   
                 <h2>{this.state.searchMonth.shiftcount} of which were between 2am and 7am.</h2>
               </React.Fragment>
+
             }
+
+  
           </div>
       </div>
     );
