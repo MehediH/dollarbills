2c34ceebc6016d93dd8ed1279854aded981aef7f
Mehedi Hassan <mehedi.dip@outlook.com>
2019-01-03 17:38:45 +0000

    i love my city lit at night

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { resolve } from 'path';
 
 class App extends Component {
   constructor(props) {
@@ -27,12 +28,38 @@ class App extends Component {
     // current month
     var date = new Date();
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-  
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+
     firstDay = firstDay.toISOString();
     lastDay = lastDay.toISOString();
 
-    fetch('https://www.thurrott.com/wp-json/wp/v2/posts?author=948&per_page=100&after=' + firstDay + '&before=' + lastDay).then((response) => response.json())
+    this.getDataMonth(firstDay, lastDay).then((data) => {
+      this.setState({
+        ...data
+      })
+    });
+
+    
+
+    // previous month
+    var prevFirstDay = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+    var prevLastDay = new Date(date.getFullYear(), date.getMonth(), 1);
+
+    prevFirstDay = prevFirstDay.toISOString();
+    prevLastDay = prevLastDay.toISOString();
+
+    this.getDataMonth(prevFirstDay, prevLastDay).then((data) => {
+      this.setState({
+        previousMonth: {
+          ...data
+        }
+      })
+    });
+  }
+
+  getDataMonth(start, end){
+
+    return fetch('https://www.thurrott.com/wp-json/wp/v2/posts?author=948&per_page=100&after=' + start + '&before=' + end).then((response) => response.json())
       .then((responseJson) => {
                 
         var tempShiftCount = 0;
@@ -55,27 +82,39 @@ class App extends Component {
 
         })
 
-        this.setState({
+        var data = {
           posts: responseJson,
           postCount: responseJson.length,
           shiftcount: tempShiftCount
-        })
+        }
+
+        return data;
 
       })
       .catch((error) => {
         console.error(error);
-      });
+      }
+    );
+
   }
 
   render() {
     return (
       <div className="app">
-          <div class="main">
+          <div className="main">
             <h1>
               You posted {this.state.postCount} articles this month.
             </h1>
 
             <h2>{this.state.shiftcount} of which were between 2am and 7am.</h2>
+          </div>
+
+          <div className="main">
+            <h1>
+              You posted {this.state.previousMonth.postCount} articles last month.
+            </h1>
+
+            <h2>{this.state.previousMonth.shiftcount} of which were between 2am and 7am.</h2>
           </div>
           
       </div>
