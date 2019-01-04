4faaf2be4d9b8d8e77904aeb48fba5f0e3b061f5
Mehedi Hassan <mehedi.dip@outlook.com>
2019-01-04 12:11:11 +0000

    BREAK DA LAW

--- src/App.js
+++ src/App.js
@@ -195,7 +195,7 @@ class App extends Component {
     return (
       <div className="app">
           <header>
-            <h1>DOLLAR BILLS 💸💰</h1>
+            <h1>DOLLAR BILLS <a onClick={this.updateAuthor}>💸💰</a></h1>
 
             <div class="search">
               <label>Author ID:</label>
